ae3b131f63e01194a13385d192b90935402e54bc
Juancode88 <juanlopzgo865@gmail.com>
2024-07-03 18:19:01 -0500

    first commit

---
+++ SecondApi/products.py
@@ -0,0 +1,7 @@
+products = [
+    {'name': 'laptop', 'price': 1200, 'quantity': 4},
+    {'name': 'mouse', 'price': 70, 'quantity': 10},
+    {'name': 'monitor', 'price': 400, 'quantity': 3},
+    {'name': 'camera', 'price': 750,'quantity': 5},
+    {'name': 'earphones','price': 750,'quantity': 4}
+]
